d732daab53e7ba20a3e156ac82b533b08579725f
manikanta2901 <manikanta2901@gmail.com>
2022-04-23 23:36:25 +0530

    23-04-2022 solutions added

--- Easy/EvenFrequency.cpp
+++ Easy/EvenFrequency.cpp
@@ -21,13 +21,13 @@ bool solve(vector<int>& nums) {
 
 bool solve(vector<int>& nums) {
     int a = 0;
-    int b = 0;
+    int b = 0,c = 0;
     for(int i = 0; i < nums.size(); i++){
         a ^= nums[i];
         b ^= nums[i] + 1;
-        a ^= num - 1;
+        c ^= num - 1;
     }
-    if(a == 0 && b == 0){
+    if(a == 0 && b == 0 && c == 0){
         return true;
     }
     return  false;
